191c41b0f68dd959793a5bf6d5243cbe2e8ccdef
inker <anton.veselyev@gmail.com>
2019-09-06 01:51:52 +0100

    add config as a param

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { unparse } from 'papaparse'
+import { unparse, UnparseConfig } from 'papaparse'
 
 import getTransformer from './getTransformer'
 
@@ -8,12 +8,20 @@ import {
   ColumnKeys,
 } from './types'
 
-const config = {
+type Config = Omit<UnparseConfig, 'columns'>
+
+const defaultConfig: Config = {
   // quotes: true,
 }
 
-export = <C extends Column>(columns: readonly C[]) => {
+export = <C extends Column>(columns: readonly C[], options: Readonly<Config> = {}) => {
+  const config: Config = {
+    ...defaultConfig,
+    ...options,
+  }
+
   const transformer = getTransformer(columns)
+
   return (list: readonly Readonly<Obj<ColumnKeys<C>>>[]) => {
     const mapped = list.map(transformer)
     return unparse(mapped, config)
